d0fb97b5bc85c39710f26043dc267f404b047d51
owner <owner@owner-PC>
2017-06-01 10:10:06 +0900

    06.01 추가 수업 내용임

---
+++ May29/src/honam/inc/oop/chap06/lib/Rect.java
@@ -0,0 +1,26 @@
+package honam.inc.oop.chap06.lib;
+
+public class Rect {
+	int width;
+	int height;
+	
+	public Rect() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Rect(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	public boolean equals(Rect p) {
+		if (width * height == p.width * p.height)
+			return true;
+		else
+			return false;
+	}
+	
+	public String toString() {
+		return "Rect";
+	}
+}
